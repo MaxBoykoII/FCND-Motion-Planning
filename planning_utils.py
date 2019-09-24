4f5554a4ecc20697eaa65135c0e8cd7f49f5f2ba
Max Boyko <maxboykomax@gmail.com>
2019-09-23 23:28:25 -0700

    Add diagonal motions to the planning algorithm.

--- planning_utils.py
+++ planning_utils.py
@@ -57,6 +57,10 @@ class Action(Enum):
     EAST = (0, 1, 1)
     NORTH = (-1, 0, 1)
     SOUTH = (1, 0, 1)
+    NORTH_WEST = (1, -1, np.sqrt(2))
+    NORTH_EAST = (1, 1, np.sqrt(2))
+    SOUTH_WEST = (-1, -1, np.sqrt(2))
+    SOUTH_EAST = (-1, 1, np.sqrt(2))
 
     @property
     def cost(self):
@@ -86,6 +90,15 @@ def valid_actions(grid, current_node):
         valid_actions.remove(Action.WEST)
     if y + 1 > m or grid[x, y + 1] == 1:
         valid_actions.remove(Action.EAST)
+   
+    if not Action.NORTH in valid_actions or not Action.WEST in valid_actions:
+        valid_actions.remove(Action.NORTH_WEST)
+    if not Action.NORTH in valid_actions or not Action.EAST in valid_actions:
+        valid_actions.remove(Action.NORTH_EAST)
+    if not Action.SOUTH in valid_actions or not Action.WEST in valid_actions:
+        valid_actions.remove(Action.SOUTH_WEST)
+    if not Action.SOUTH in valid_actions or not Action.EAST in valid_actions:
+        valid_actions.remove(Action.SOUTH_EAST)
 
     return valid_actions
 
